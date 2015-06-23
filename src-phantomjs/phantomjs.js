ffb9e325bce55e73150e49f747275ae156c4c592
Andreas <ciscoheat@gmail.com>
2015-06-23 15:09:33 +0200

    PhantomJs support

---
+++ src-phantomjs/phantomjs.js
@@ -0,0 +1,17 @@
+var page = require('webpage').create();
+var args = require('system').args;
+var successMsg = args.length > 1 && args[1] ? args[1] : null;
+
+page.onConsoleMessage = function(msg) {
+  if(successMsg !== null && successMsg === msg) phantom.exit(0);
+};
+
+page.open('bin/phantomjs.html', function(status) {
+  // If we arrive here, no successMsg has been found in console.log.
+  var success = status === 'success' && successMsg === null;
+  phantom.exit(success ? 0 : 1);
+
+  // If you want to search for successMsg in document.body instead:
+  // var result = page.evaluate(function() { return document.body.innerText; });
+  // phantom.exit(success || result.indexOf(successMsg) >= 0 ? 0 : 1);
+});
