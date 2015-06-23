e00ffed57ee0df3f053f82b90d8888822fd77960
Andreas <ciscoheat@gmail.com>
2015-06-23 15:30:51 +0200

    Renamed build output.

--- .travis-phantomjs-script.sh
+++ .travis-phantomjs-script.sh
@@ -4,7 +4,7 @@
 # $1 is the compiled js, relative to "bin"
 # $2 (optional) is a trace/console success message to look for.
 
-# Copy html file to bin
+# Copy html file to bin and replace the javascript reference
 cp src-phantomjs/phantomjs.html bin/phantomjs.html
 sed -i "s/\[PHANTOMJS\]/$1/g" bin/phantomjs.html
 
