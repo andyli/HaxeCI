b42c7cb2cc4d167a07073858054cf71587746df1
Andy Li <andy@onthewings.net>
2016-03-22 16:17:52 +0800

    update flash player version to 21

--- flash/Install.hx
+++ flash/Install.hx
@@ -10,9 +10,9 @@ class Install {
 		case "Linux":
 			"https://fpdownload.macromedia.com/pub/flashplayer/updaters/11/flashplayer_11_sa_debug.i386.tar.gz";
 		case "Mac":
-			"https://fpdownload.macromedia.com/pub/flashplayer/updaters/19/flashplayer_19_sa_debug.dmg";
+			"https://fpdownload.macromedia.com/pub/flashplayer/updaters/21/flashplayer_21_sa_debug.dmg";
 		case "Windows":
-			"http://fpdownload.macromedia.com/pub/flashplayer/updaters/19/flashplayer_19_sa_debug.exe";
+			"http://fpdownload.macromedia.com/pub/flashplayer/updaters/21/flashplayer_21_sa_debug.exe";
 		case _:
 			throw "unsupported system";
 	}
